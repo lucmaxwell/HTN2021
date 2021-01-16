8a9b225e2472a9779a6f0afe7e042544ccd12299
Luc <lucmaxwell8@gmail.com>
2021-01-16 02:33:31 -0600

    Added basic GUI

--- main.py
+++ main.py
@@ -21,35 +21,46 @@ Loose groups and teams for the project
 import numpy as np
 import pandas as pd
 import tkinter as tk
+from tkinter import messagebox
 from datetime import datetime
 import statistics
+
 
 root = tk.Tk()
 root.iconbitmap(r'icon.ico')
 root.geometry("500x500")
 root.resizable(True, True)
 root.title("Stock tracker")
-root.configure(bg = "#cdcdcd")
+root.configure(bg = "#a2a2a2")
+
+
+def acceptFile():
+    fileName = fileInput.get()
+    data = ""
+    try:
+        data = pd.read_csv(fileName)
+    except Exception as e:
+        messagebox.showerror(title = "Invalid file", message = "Please ensure file is in current working directory and the correct name was entered!")
+        print(e)
+
+fileInput = tk.Entry(root, width = 55, bd = 2, justify = "left", font = "TkDefault 10")
+fileInput.grid(row = 0, column = 0)
+
+fileAccept = tk.Button(root, text = "Submit", font = "TkDefault 10", command = acceptFile)
+fileAccept.grid(row = 0, column = 1)
+
+
 
 
 root.mainloop()
 
 
-
-#Initialize file and convert it into data-frame
-
-fold = "C:\\Users\\User\\Documents\\Xingming\\UW\\Projects\\HTN\\"
-    ## replace with your own folder
-file = fold + "SPY.csv"
-
-spy_data = pd.read_csv(file) # read into pandas dataframe
-
 ## convert date into some numeric variable from a given origin
 ## i think shifting 1993-01-29 to 0 and 2021-01-14 to 7041 should be good
-r = datetime.strptime("1993-02-01", "%Y-%m-%d")
+#r = datetime.strptime("1993-02-01", "%Y-%m-%d")
 
-for i in range(len(spy_data)):
-    spy_data.Date[i] = datetime.strptime(spy_data.Date[i], "%Y-%m-%d")
+#for i in range(len(spy_data)):
+#    spy_data.Date[i] = datetime.strptime(spy_data.Date[i], "%Y-%m-%d")
 
 ## this is questionably done
 ## alternatively this could be done with the time package but idk about
