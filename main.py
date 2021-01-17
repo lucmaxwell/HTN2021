53a74643c2791cb5fd300cdbe5854197ed38f006
Luc <lucmaxwell8@gmail.com>
2021-01-16 21:12:45 -0600

    added organized data

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ Different teams for the project (see discord for the rest of the information)
 3. AI and predictive analysis (tensorflow and scikit)
 
 """
-
-
 import numpy as np
 import pandas as pd
 import tkinter as tk
-from tkinter import *
 from tkinter import messagebox
 from matplotlib import pyplot as pp
-from datetime import datetime
+from datetime import datetime, date, timedelta
 import statistics
-#from matplotlib import pyplot
+from dateutil.relativedelta import relativedelta
 
 # Windows settings
 root = tk.Tk()
@@ -26,12 +23,12 @@ root.resizable(True, True)
 root.title("Stock tracker")
 root.configure(bg = "#a2a2a2")
 
-class date:
+class point:
     def __init__(self, colData):
-        date, self.open, self.high, self.low, self.close, self.adjClose, self.volume = colData
-        self.year, self.month, self.day = date.split('-')
+        dates, self.open, self.high, self.low, self.close, self.adjClose, self.volume = colData
+        year, month, day = dates.split('-')
+        self.date = date(int(year), int(month), int(day))
         
-
 # Reading the data files (format Datasets\File.csv)
 def acceptFile(fileName):
     try:
@@ -41,14 +38,42 @@ def acceptFile(fileName):
         messagebox.showerror(title = "Invalid file", message = "Please ensure file is in current working directory and the correct name was entered!")
 
 
-# Finding the corresponding data file for the current ticker + time period
+def subtractDay(dateObj, num):
+    return dateObj - timedelta(num)
+
+def subtractMonth(dateObj, num):
+    return dateObj - relativedelta(months=num)
+
+def subtractYear(dateObj, num):
+    return dateObj - relativedelta(years=num)
+
+
 def searchTicker():
     ticker = fileInput.get()
     data = acceptFile('SPY_max.csv').transpose() # change to ticker at the end. Changed to  for testing
     dateObj = []
     for colNum, colData in data.iteritems():
-        obj = date(colData)
+        obj = point(colData)
         dateObj.append(obj)
+
+    dataDates = date.today()
+
+    dateSelection = optionList.get()
+    if dateSelection[1] == 'D':
+        dataDates = subtractDay(dataDates, int(dateSelection[0]))
+    elif dateSelection[1] == 'W':
+        dataDates = subtractDay(dataDates, int(dateSelection[0])*7)
+    elif dateSelection[1] == 'M':
+        dataDates = subtractMonth(dataDates, int(dateSelection[0]))
+    elif dateSelection[1] == 'Y':
+        dataDates = subtractYear(dataDates, int(dateSelection[0]))
+    else:
+        dataDates = date(1, 1, 1)
+    
+    filteredData = set()
+    for obj in dateObj:
+        if obj.date >= dataDates:
+            filteredData.add(obj)
 
 
 # Frames for organization
@@ -56,48 +81,29 @@ topFrame = tk.Frame(master = root)
 bodyFrame = tk.Frame(master = root)
 
 # Text box for stock ticker
-# TODO implement smart search of ticker to generate file
 fileInput = tk.Entry(master = topFrame, width = 55, bd = 2, justify = "left", font = "TkDefault 10")
 fileInput.grid(row = 0, column = 0)
 
 # Drop down box to add time frame
 timeOptions = ["1D", "1W", "1M", "3M", "6M", "1Y", "5Y", "max"]
-optionList = StringVar(topFrame)
+optionList = tk.StringVar(topFrame)
 optionList.set(timeOptions[0])
 timePeriod = tk.OptionMenu(topFrame, optionList, *timeOptions)
 timePeriod.config(width = 3, font = "TKDefault 10")
 timePeriod.grid(row = 0, column = 1)
 
-
 # Button to commence search for data
 fileAccept = tk.Button(master = topFrame, text = "Submit", font = "TkDefault 10", command = searchTicker)
 fileAccept.grid(row = 0, column = 2)
-
 
 topFrame.pack()
 bodyFrame.pack()
 
 root.mainloop()
 
-
-
-
-# Temporary definition of Day class
-class Day:
-    def __init__(self, day, month, year, open, high, low, close, volume):
-        self.day = day
-        self.month = month
-        self.year = year
-        self.open = open
-        self.high = high
-        self.low = low
-        self.close = close
-        self.volume = volume
-        self.date = "" + day + "/" + month + "/" + year
-
-
+"""
 # Temporary place for graphing utility
-# scope is a listof Day
+# scope is a list of Day
 def graphData(scope):
     x_values = []
     y_values = []
@@ -111,3 +117,4 @@ def graphData(scope):
 
     pp.plot(x_values, y_values)
     pp.show()
+"""
